d1b17bc45e57481baf3bf5e083f28be84afd0eff
pkihiu <p.kihiu@alustudent.com>
2023-07-06 04:42:46 -0700

    two

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 'Write a new programme with anew line'
-print('"Programming is like building a multilingual puzzle \n')
+print('\"Programming is like building a multilingual puzzle')
