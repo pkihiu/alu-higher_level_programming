95db0780cf5825109234d9aeb5d95acf3bafba3c
pkihiu <p.kihiu@alustudent.com>
2023-07-10 10:37:58 -0700

    nine

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
-    unsigned_num = abs(number)
-    last_digit = unsigned_num % 10
-    print(f"{last_digit}", end="")
-    return(last_digit)
+    if number >= 0:
+        n = number % 10
+        print("{:d}".format(n), end="")
+        return n
+    else:
+        number = -number
+        n = number % 10
+        number = -number
+        print("{:d}".format(n), end="")
+        return n
