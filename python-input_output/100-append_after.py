28f72c353c910e5c13bbdc6e2c1ee460a02f3d8e
pkihiu <p.kihiu@alustudent.com>
2023-07-27 13:50:32 -0700

    ten

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Defining a function to insert a line to a text"""
+'''Defines function append_after
+'''
+
 
 def append_after(filename="", search_string="", new_string=""):
-    """Insering a line of text to a file
-
-    args:
-        filename (str): The name of the file.
-        search_string (str): The string to search for within the file.
-        new_string (str): The string to insert.
-    """
-    text = ""
-    with open(filename) as r:
-        for line in r:
-            text += line
+    ''' a function that inserts a line of text to a file,
+    after each line containing a specific string.
+    '''
+    s = ""
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            s += line
             if search_string in line:
-                text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+                s += new_string
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(s)
