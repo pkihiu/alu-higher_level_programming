86b84608d072d2cad7c9be0e3bad8734970f33e6
pkihiu <p.kihiu@alustudent.com>
2023-08-01 08:31:01 -0700

    SQL

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
-DROP DTABASE IF EXISTS hbtn_0c_0;
+-- Write a script that deletes the database 'hbtn_0c_0' in your MySQL server.
+DROP DTABASE IF EXISTS 'hbtn_0c_0' ;
