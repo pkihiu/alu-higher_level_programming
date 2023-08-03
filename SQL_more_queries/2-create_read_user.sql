a64296ee98de520dba485c29e2ae3bae41d92aff
pkihiu <p.kihiu@alustudent.com>
2023-08-03 14:15:47 -0700

    two

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
--- MrSQL script to create Database and User
+-- creating a dabases and adding a use
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.*
-TO user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
