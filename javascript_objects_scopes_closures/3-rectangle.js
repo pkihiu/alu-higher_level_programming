856214e5fc2cd992fa7e91b339b0365f36953922
pkihiu <p.kihiu@alustudent.com>
2023-10-01 03:34:14 -0700

    Upadate javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
-// JS script to define 
+// JS script to define a class, creates an instance method to print rectangle
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
       this.width = w;
       this.height = h;
     }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
+    for (let count = 0; count < this.height; count++) {
       console.log('X'.repeat(this.width));
     }
   }
 }
-
 module.exports = Rectangle;
